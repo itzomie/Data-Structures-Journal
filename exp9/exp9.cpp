08e2584c68d49465fc2f94e645038e23072c910f
itzomie <58023433+itzomie@users.noreply.github.com>
2020-05-19 22:52:31 +0530

    Add files via upload

---
+++ exp9/exp9.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int arr[20],n,x,i,flag=0;
+cout<<"How many elements? \n";
+cin>>n;
+cout<<"\nEnter elements of the array\n";
+for(i=0;i<n;++i)
+cin>>arr[i];
+cout<<"\nEnter element to be searched: ";
+cin>>x;
+for(i=0;i<n;++i)
+{ if(arr[i]==x)
+{ flag=1;
+ break;
+}
+}
+if(flag)
+cout<<"\nElement is found at position "<<i+1;
+else
+cout<<"\nElement not found!";
+return 0;
+}
